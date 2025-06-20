62b4c20852c70c706f127c829daf1bbdc325782e
Rafael Oliveira <103541128+rafaloliveira@users.noreply.github.com>
2025-06-20 10:27:13 -0300

    Atualização

--- main.py
+++ main.py
@@ -1601,17 +1601,16 @@ if st.session_state.aba_ativa == "aba3":
 
                     except Exception as e:
                         st.warning(f"⚠️ Erro ao montar card de ocorrência: {e}")
-                        html_card = f"""
+                        html_card = """
                         <div style='background-color:gray;padding:10px;border-radius:10px;color:white;
                         box-shadow: 0 4px 10px rgba(0,0,0,0.3);margin-bottom:5px;min-height:250px;font-size:15px;'>
-
                         <strong>⚠️ Erro ao carregar ticket</strong><br>
                         Verifique os dados no Supabase ou tente novamente.
                         </div>
                         """
+                    finally:
+                        st.markdown(html_card, unsafe_allow_html=True)
 
-                    # Exibir o card (sempre definido, com ou sem erro)
-                    st.markdown(html_card, unsafe_allow_html=True)
 
 
 # =========================
